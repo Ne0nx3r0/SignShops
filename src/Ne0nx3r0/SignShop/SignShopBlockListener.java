e86f814d75c6457ae4fca04756fbba8b87a73426
Ne0nx3r0 <Ne0nx3r0@gmail.com>
2011-05-02 22:14:56 -0500

    sdf

---
+++ src/Ne0nx3r0/SignShop/SignShopBlockListener.java
@@ -0,0 +1,30 @@
+package Ne0nx3r0.SignShop;
+
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.Material;
+
+public class SignShopBlockListener extends BlockListener{
+    private final SignShop plugin;
+
+    public SignShopBlockListener(SignShop instance){
+        this.plugin = instance;
+    }
+
+    @Override
+    public void onBlockBreak(BlockBreakEvent event){
+        if(event.getBlock().getType() == Material.WALL_SIGN
+        || event.getBlock().getType() == Material.SIGN_POST){
+            plugin.Storage.removeSeller(event.getBlock().getLocation());
+        }
+    }
+
+    @Override
+    public void onBlockBurn(BlockBurnEvent event){
+        if(event.getBlock().getType() == Material.WALL_SIGN
+        || event.getBlock().getType() == Material.SIGN_POST){
+            plugin.Storage.removeSeller(event.getBlock().getLocation());
+        }
+    }
+}
